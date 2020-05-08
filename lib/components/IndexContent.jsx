5772ad06294751907180f3eed55f27b00d98243b
Charles Bergeron <chuck.bergeron@gmail.com>
2020-05-08 15:12:37 -0700

    * iterate on index

--- lib/components/IndexContent.jsx
+++ lib/components/IndexContent.jsx
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Link from 'next/link'
 
-import { WalletContext } from 'lib/components/WalletContextProvider'
 import { getDefaultPoolContractAddress } from 'lib/utils/getDefaultPoolContractAddress'
 
 export const IndexContent = (
@@ -10,17 +9,51 @@ export const IndexContent = (
   const kovanPoolContractAddress = getDefaultPoolContractAddress('kovan')
 
   return <>
-    TODO: Form to enter pool address or use link to default pool for this network:
-    <hr/>
-    Kovan:
-    <br />
-    <Link
-      href='/pools/[networkName]/[poolAddress]'
-      as={`/pools/kovan/${kovanPoolContractAddress}`}
+    <div
+      className='text-lg sm:text-xl lg:text-2xl mb-4'
     >
-      <a>
-        {kovanPoolContractAddress}
-      </a>
-    </Link>
+      View one of the default pools:
+    </div>
+
+    <div
+      className='text-xs sm:text-lg lg:text-xl'
+    >
+      <Link
+        href='/pools/[networkName]/[poolAddress]'
+        as={`/pools/kovan/${kovanPoolContractAddress}`}
+      >
+        <a>
+          <span className='text-blue-200 text-base'>  Default Kovan Pool</span>
+          <br/>
+          {kovanPoolContractAddress}
+        </a>
+      </Link>
+    </div>
+
+
+    <hr/>
+
+    <div
+      className='text-lg sm:text-xl lg:text-2xl mb-4'
+    >
+      Or enter a pool to view's details:
+    </div>
+
+    <form
+      handleSubmit={(e) => {
+        e.preventDefault()
+      }}
+    >
+      <label htmlFor=''>Pool the network is on:</label>
+      radio
+
+      <label htmlFor=''>Pool's contract address:</label>
+      <input />
+
+      <button>
+        
+      </button>
+    </form>
+  
   </>
 }
