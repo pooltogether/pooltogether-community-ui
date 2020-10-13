85be388b099eec6cf17b589efe4502d67d710612
Brendan Asselstine <mail.asselstine@gmail.com>
2020-10-13 16:16:04 -0700

    Reference Pool app is now aware of the PermitAndDepositDai contract
    - also fixed weird localhost bug

--- lib/components/UserActionsUI.jsx
+++ lib/components/UserActionsUI.jsx
@@ -2,24 +2,33 @@ import React from 'react'
 
 import { FormPanel } from 'lib/components/FormPanel'
 import { DepositUI } from 'lib/components/DepositUI'
+import { PermitAndDepositUI } from 'lib/components/PermitAndDepositUI'
 import { SweepTimelockedUI } from 'lib/components/SweepTimelockedUI'
 import { UnlockDepositUI } from 'lib/components/UnlockDepositUI'
 import { WithdrawUI } from 'lib/components/WithdrawUI'
 
 export const UserActionsUI = (props) => {
+  let depositUI
+  if (props.poolAddresses && props.poolAddresses.token.toLowerCase() === '0x6b175474e89094c44da98b954eedeac495271d0f') {
+    depositUI = 
+      <PermitAndDepositUI
+          {...props}
+        />
+  } else {
+    depositUI = props.usersChainValues.usersTokenAllowance.gt(0) ?
+      <DepositUI
+        {...props}
+      /> :
+      <UnlockDepositUI
+        {...props}
+      />
+  }
   return <>
     <div
       className='flex flex-col sm:flex-row'
     >
       <FormPanel>
-        {props.usersChainValues.usersTokenAllowance.gt(0) ?
-          <DepositUI
-            {...props}
-          /> :
-          <UnlockDepositUI
-            {...props}
-          />
-        }
+        {depositUI}
       </FormPanel>
       
       <FormPanel>
