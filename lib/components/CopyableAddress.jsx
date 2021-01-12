c8749d876a3d01bfd1be140048d3395596cc36e0
Dylan DesRosier <dylan@pooltogether.com>
2021-01-12 18:11:12 -0500

    add steps for prizes. Add snx and aave images.

---
+++ lib/components/CopyableAddress.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import FeatherIcon from 'feather-icons-react'
+import classnames from 'classnames'
+import CopyToClipboard from 'react-copy-to-clipboard'
+
+import { shorten } from 'lib/utils/shorten'
+import { poolToast } from 'lib/utils/poolToast'
+
+export const CopyableAddress = (props) => {
+  const { address, className } = props
+
+  const handleCopy = () => {
+    poolToast.success('Copied to clipboard')
+  }
+
+  return (
+    <CopyToClipboard text={address} onCopy={handleCopy}>
+      <div className={classnames('cursor-pointer', className)}>
+        <span className='hidden sm:flex'>
+          {address}
+          <FeatherIcon
+            icon='copy'
+            className='ml-2 sm:ml-4 my-auto w-3 h-3 sm:w-4 sm:h-4 my-auto stroke-current'
+          />
+        </span>
+        <span className='flex sm:hidden'>
+          {shorten(address)}
+          <FeatherIcon
+            icon='copy'
+            className='ml-2 sm:ml-4 my-auto w-3 h-3 sm:w-4 sm:h-4 my-auto stroke-current'
+          />
+        </span>
+      </div>
+    </CopyToClipboard>
+  )
+}
