e1704e7ccc6178e787a1d058321581e4ac636b5f
Charles Bergeron <chuck.bergeron@gmail.com>
2021-03-31 14:11:11 -0700

    * adding external dep for chainId info * bring in images for networks * fixing bugs

--- lib/components/UnsupportedNetwork.jsx
+++ lib/components/UnsupportedNetwork.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import classnames from 'classnames'
 
 import { SUPPORTED_NETWORKS } from 'lib/constants'
-import { chainIdToName, networkColorClassname } from 'lib/utils/networks'
+import { chainIdToView, networkColorClassname } from 'lib/utils/networks'
 
 export const UnsupportedNetwork = (props) => {
   const { chainId } = props
   return (
-    <div className='flex flex-col text-center my-auto'>
-      <h1 className='mb-8'>⚠️ Unsupported network ⚠️</h1>
-      <div>
+    <div className='flex flex-col'>
+      <h3 className='mb-4'>⚠️ Unsupported network ⚠️</h3>
+      <div className='mb-1'>
         You're currently connected to <Network chainId={chainId} />.
       </div>
       <div className='mb-4'>
@@ -19,7 +19,7 @@ export const UnsupportedNetwork = (props) => {
         {SUPPORTED_NETWORKS.map((network) => {
           if ([31337, 1234].includes(network)) return null
           return (
-            <li className='ml-2 xs:ml-4' key={network}>
+            <li key={network}>
               <Network chainId={network} />
             </li>
           )
@@ -29,9 +29,13 @@ export const UnsupportedNetwork = (props) => {
   )
 }
 
-const Network = (props) => (
-  <span className={classnames('capitalize', networkColorClassname(props.chainId))}>
-    <b>{chainIdToName(props.chainId)}</b>
-    <small className='ml-1'>{`network id: ${props.chainId}`}</small>
-  </span>
-)
+const Network = (props) => {
+  const viewName = chainIdToView(props.chainId)
+
+  return (
+    <span className={classnames(networkColorClassname(props.chainId))}>
+      <b>{viewName || 'an unknown network'}</b>
+      <small className='ml-1 text-highlight-1'>{`chainId: ${props.chainId}`}</small>
+    </span>
+  )
+}
