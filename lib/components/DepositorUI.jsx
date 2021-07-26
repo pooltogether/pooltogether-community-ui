12f8c34ddfeef22586a8928d2a9a255a5489a09c
Charles Bergeron <chuck.bergeron@gmail.com>
2021-07-26 16:38:32 -0700

    * always show the same links at the bottom of the pool pages, and fix the links that point to /admin (which doesn't exist, as it's /manage#admin)

--- lib/components/DepositorUI.jsx
+++ lib/components/DepositorUI.jsx
@@ -6,8 +6,8 @@ import { Collapse } from 'lib/components/Collapse'
 import { DepositUI } from 'lib/components/DepositUI'
 import { PrizeCard } from 'lib/components/PrizeCard'
 import { WithdrawUI } from 'lib/components/WithdrawUI'
-import { RelativeInternalLink } from 'lib/components/RelativeInternalLink'
-import { PoolDepositorStatsCard, UsersStatsCard } from 'lib/components/PoolStats'
+import { RelativeNavLinks } from 'lib/components/RelativeNavLinks'
+import { PoolDepositorStatsCard } from 'lib/components/PoolStats'
 import { useTimeLeftBeforePrize } from 'lib/hooks/useTimeLeftBeforePrize'
 
 export const DepositorUI = (props) => {
@@ -29,10 +29,7 @@ export const DepositorUI = (props) => {
         </Collapse>
       </Card>
 
-      <div className='flex justify-between w-3/4 sm:w-1/2 mx-auto'>
-        <RelativeInternalLink link='/manage'>View Pool Details</RelativeInternalLink>
-        <RelativeInternalLink link='/admin'>Manage Pool</RelativeInternalLink>
-      </div>
+      <RelativeNavLinks />
     </>
   )
 }
